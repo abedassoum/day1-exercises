24fae64bad710b09a7cd34dd944e45f2cb46925f
Abed Rass <abd.assoum@outlook.com>
2024-02-28 18:04:48 +0100

    Ex 6 solved

--- src/exercises/UseEffect.tsx
+++ src/exercises/UseEffect.tsx
@@ -2,30 +2,39 @@ import { useEffect, useState } from "react";
 import { BaseProps } from "../types";
 
 
-/*
-Exercise
-part one --> provide the useEffect without a dependency array and clearInterval and see what happens
-part two --> provide the useEffect with an empty dependency array and see what happens
-part three --> add an return function  to clear the interval ()
-part four --> Add a button to start and stop the interval via a boolean state variable to see what happens with values in the dependency array
-*/
+
 export default function UseEffectDemo({ title }: BaseProps) {
   const [count, setCount] = useState(0);
+  const [shouldCount, setShouldCount] = useState(false);
 
 
-  useEffect(() => {
-    const i = setInterval(() => {
-      setCount((prev) => prev + 1);
-    }, 1000);
-  }, );
+
+      useEffect(() => {
+        if (shouldCount) {
+          const i = setInterval(() => {
+            setCount((prev) => prev + 1);
+          }, 1000);
+          return () => {
+            clearInterval(i);
+          };
+        }
+      }, [shouldCount]);
+  
 
 
   return (
     <>
       <h2>{title}</h2>
       <p>{count}</p>
+      <button onClick={() => setShouldCount((prev) => !prev)}>
+     {shouldCount ? "Stop Count" : "Start Count"}
+   </button>
     </>
   );
 }
 
+
+function setCount(arg0: (prev: any) => any) {
+  throw new Error("Function not implemented.");
+}
 
